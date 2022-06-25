8b8220551f370f17a182419a78e49944809ffdf9
MohakKala07 <84536833+MohakKala07@users.noreply.github.com>
2022-06-25 12:58:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 import mediapipe as mp
 import tensorflow as tf
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 
 # initialize mediapipe
 mpHands = mp.solutions.hands
